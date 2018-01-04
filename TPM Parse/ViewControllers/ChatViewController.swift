815c714ddaa8bd844d1902c4e3657184d0d01804
sammanthp007 <sammanbikramthapa@gmail.com>
2018-01-04 10:43:59 +0545

    add chatViewController with aito layout

--- TPM Parse/ViewControllers/ChatViewController.swift
+++ TPM Parse/ViewControllers/ChatViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ChatViewController: UIViewController {
 
+    @IBOutlet weak var chatMessageTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -19,6 +22,11 @@ class ChatViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func onSendTap(_ sender: Any) {
+        print ("chat sending")
     }
     
 
